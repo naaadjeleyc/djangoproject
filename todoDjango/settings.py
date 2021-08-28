77ea42a1b5ceae53161c32f3cd1f5097df56fe92
naaadjeleyc <naaadjeleyc@gmail.com>
2021-08-28 04:14:42 +0100

    adding forms.py

--- todoDjango/settings.py
+++ todoDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q+qamlst@(@xzshh)jutog2zgt!mktfwv(7du2%(sp@!ffi3yr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
