3b1a301f1e58507b9bc32303604bb98e6fa7a0d6
naaadjeleyc <naaadjeleyc@gmail.com>
2021-08-30 15:28:12 +0100

    adding delete button

--- todo_list/views.py
+++ todo_list/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 def home(request):
     if request.method == 'POST':
         form = ListForm(request.POST or None)
-        
+
         if form.is_valid():
             form.save()
             all_items = List.objects.all
@@ -16,3 +16,9 @@ def home(request):
     else:
         all_items = List.objects.all
         return render(request, 'home.html', {'all_items': all_items})
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request,('Item has been deleted'))
+    return redirect('home')
